376a9ca95dc55b228b0fc0882cd608b88adb4903
John Dave Camarin <67702382+jdcamarin2@users.noreply.github.com>
2020-11-28 17:29:56 +0800

    sas

--- propel/lib/main.dart
+++ propel/lib/main.dart
@@ -1,241 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:propel/utilities/constants.dart';
-import 'package:propel/screens/signup.dart';
-void main() => runApp(MyApp());
+import 'package:propel/widgets/curved_navigation_bar.dart';
+import 'package:propel/screens/SingIn.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+//main method
+void main() => runApp(MaterialApp(home: BottomNavBar()));
+
+class BottomNavBar extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        //home: SignUpScreen());
-        //home: LoginScreen());
-        routes: {
-          '/': (context) => LoginScreen(),
-          '/details': (context) => SignUpScreen(),
-        }
-    );
-  }
+  _BottomNavBarState createState() => _BottomNavBarState();
 }
 
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _page = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
 
-
-//Log In Page
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  bool _rememberMe = false;
-
-  Widget _buildEmailTF() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'Email',
-          style: kLabelStyle,
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.email,
-                color: Colors.white,
-              ),
-              hintText: 'Username',
-              hintStyle: kHintTextStyle,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildPasswordTF() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'Password',
-          style: kLabelStyle,
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextField(
-            obscureText: true,
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.lock,
-                color: Colors.white,
-              ),
-              hintText: 'Password',
-              hintStyle: kHintTextStyle,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildForgotPasswordBtn() {
-    return Container(
-      alignment: Alignment.centerLeft,
-      child: FlatButton(
-        onPressed: () => print('Forgot Password Button Pressed'),
-        padding: EdgeInsets.only(left: 0.0),
-        child: Text(
-          'Forgot Password?',
-          style: kLabelStyle,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildLoginBtn() {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 25.0),
-      width: double.infinity,
-      child: RaisedButton(
-        elevation: 5.0,
-        onPressed: () => print('Login Button Pressed'),
-        padding: EdgeInsets.all(15.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30.0),
-        ),
-        color: Colors.white,
-        child: Text(
-          'LOGIN',
-          style: TextStyle(
-            color: Colors.blue,
-            letterSpacing: 1.5,
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'OpenSans',
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSignupBtn() {
-
-    return GestureDetector(
-      onTap: ()  => Navigator.push(context, MaterialPageRoute(builder: (context)=> SignUpScreen())),
-      child: RichText(
-        text: TextSpan(
-          children: [
-            TextSpan(
-              text: 'Don\'t have an Account? ',
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 18.0,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            TextSpan(
-              text: 'Sign Up',
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  List<Widget> _widgetOptions = <Widget>[
+    Text('Trending'),
+    Text('Messgaes Screen'),
+    Text('Profile Screen'),
+    SignIn()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0x6dd5ed),
-                      Color(0xffffff),
-                      Color(0xffffff),
-                      Color(0xffffff),
-                    ],
-                    stops: [0.3, 0.4, 0.7, 1.1],
-                  ),
-                ),
-              ),
-              Container(
-                height: double.infinity,
-                child: SingleChildScrollView(
-                  physics: AlwaysScrollableScrollPhysics(),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 40.0,
-                    vertical: 120.0,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'propel',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontFamily: 'OpenSans',
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
 
-                        ),
-                      ),
-                      SizedBox(height: 30.0),
-                      _buildEmailTF(),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      _buildPasswordTF(),
-                      _buildForgotPasswordBtn(),
-                      //  _buildRememberMeCheckbox(),
-                      _buildLoginBtn(),
-                      _buildSignupBtn(),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
+        bottomNavigationBar: CurvedNavigationBar(
+          key: _bottomNavigationKey,
+          index: 0,
+          height: 60.0,
+          items: <Widget>[
+            Icon(
+              Icons.accessibility_new_outlined,
+              size: 30,
+              color: Colors.blueAccent,
+            ),
+            Icon(Icons.archive_outlined, size: 30, color: Colors.blueAccent),
+            Icon(Icons.apps_rounded, size: 30, color: Colors.blueAccent),
+            Icon(Icons.account_box_outlined,
+                size: 30, color: Colors.blueAccent),
+          ],
+          color: Colors.white,
+          buttonBackgroundColor: Colors.white,
+          backgroundColor: Colors.blue,
+          animationCurve: Curves.decelerate,
+          animationDuration: Duration(milliseconds: 500),
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          },
         ),
-      ),
-    );
+        body: Center(
+          child: _widgetOptions.elementAt(_page),
+        ));
   }
 }
-
--- propel/lib/screens/signup.dart
+++ propel/lib/screens/signup.dart
@@ -1,64 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(SignUpScreen());
-
-class SignUpScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign Up",textAlign: TextAlign.center,),
-      ),
-      body: MyCustomForm(),
-
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
 
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-// Define a corresponding State class.
-// This class holds data related to the form.
-class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a `GlobalKey<FormState>`,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
+class _MyAppState extends State<MyApp> {
+  TextEditingController nameController = TextEditingController();
+  String fullName = '';
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-        key: _formKey,
-        child: Column(
-            children: <Widget>[
-              // Add TextFormFields and ElevatedButton here
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: 'What do people call you?',
-                  labelText: 'Name *',
-                ),
-                onSaved: (String value) {
-                  // This optional block of code can be used to run
-                  // code when the user saves the form.
-                },
-                validator: (String value) {
-                  return value.contains('@') ? 'Do not use the @ char.' : null;
-                },
-              )
-              
-            ]
+    return MaterialApp(
+      home: Scaffold(
 
-          ));
+          body:
+          Center(child: SingleChildScrollView (child: Column(children: <Widget>[
+            Container(
+                margin: EdgeInsets.all(10),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Full Name',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+                      fullName = text;
+                      //you can access nameController in its scope to get
+                      // the value of text entered as shown below
+                      //fullName = nameController.text;
+                    });
+                  },
+                )),
+            Container(
+              margin: EdgeInsets.all(30),
+              child: Text(fullName),
+            )
+      ]))),
+    ));
   }
 }
-
