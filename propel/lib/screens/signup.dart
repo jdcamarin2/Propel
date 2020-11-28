c2619defb8fcde4ce6e70ebb417db27fbe2d6235
John Dave Camarin <67702382+jdcamarin2@users.noreply.github.com>
2020-11-28 16:18:30 +0800

    Update signup.dart

--- propel/lib/screens/signup.dart
+++ propel/lib/screens/signup.dart
@@ -7,9 +7,58 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign Up"),
+        title: Text("Sign Up",textAlign: TextAlign.center,),
       ),
-      body: Center(),
+      body: MyCustomForm(),
+
     );
   }
 }
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+        key: _formKey,
+        child: Column(
+            children: <Widget>[
+              // Add TextFormFields and ElevatedButton here
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'What do people call you?',
+                  labelText: 'Name *',
+                ),
+                onSaved: (String value) {
+                  // This optional block of code can be used to run
+                  // code when the user saves the form.
+                },
+                validator: (String value) {
+                  return value.contains('@') ? 'Do not use the @ char.' : null;
+                },
+              )
+              
+            ]
+
+          ));
+  }
+}
+
