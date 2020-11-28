4be57b2b05db7cead324ec713f09d386f4421734
John Dave Camarin <67702382+jdcamarin2@users.noreply.github.com>
2020-11-29 06:58:53 +0800

    hgtfh

--- propel/lib/screens/signup.dart
+++ propel/lib/screens/signup.dart
@@ -19,11 +19,22 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
 
           body:
-          Center(child: SingleChildScrollView (child: Column(children: <Widget>[
-            Container(
+         Container( child: SingleChildScrollView (child: Column(children: <Widget>[
+
+           Container(
+               alignment: Alignment.center,
+               padding: EdgeInsets.all(10),
+               child: Text(
+                 'Propel',
+                 style: TextStyle(
+                     color: Colors.blue,
+                     fontWeight: FontWeight.w500,
+                     fontSize: 30),
+               )),
+           Container(
                 margin: EdgeInsets.all(10),
                 child: TextField(
-                 
+
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Full Name',
@@ -59,9 +70,20 @@ class _MyAppState extends State<MyApp> {
                     border: OutlineInputBorder(),
                     labelText: 'Password',
                   ),
-                ))
+                )),
+          Container(
+              height: 50,
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.blue,
+                child: Text('Sign Up'),
+                onPressed: () {
 
-      ]))),
-    ));
+                },
+              )
+          )
+      ])))),
+    );
   }
 }
